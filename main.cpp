9c73f5a11a94cc696033fa5b60a48cce8e53ecab
ChickkenGoat <kheawkhumhassadin@gmail.com>
2026-03-23 08:42:14 +0000

    Function

--- main.cpp
+++ main.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
-using namespace std;
-
+#include <string>
+#include <cstdlib>
 #include "student.h"
-#define N 3
+
 int main(int argc, char* argv[]) {
-  student s1,s2;
+    
+    int n_people = (argc - 1) / 2;
 
- s1.set_name("Amy"); s1.set_age(20); s2.set_name("James"); s2.set_age(17); s1.get_age()>s2.get_age() ? s1.print_name(); cout<< "is older":s2.print_name(); cout <<"is older";
+    if (n_people == 0) {
+        cout << "No data provided." << endl;
+        return 1;
+    }
 
-
-
-  student a[N];
-  // Set age and set name for the first two ppl
-  int i,age;
-  string n;
-   // 1. Ask for N names and N age using cin
- 
-  
- 
-  //2. Print name and age of all N ppl
-  
-  
-  
-  //3. Print name and age of 
-  //the youngest person
-  //Find what the age of the youngest person
- 
-  
-  // Print all info for the yougest person
- 
-  
-  
-  //4. Change input from cin to argv
-
-  
- return 0;
+    student* a = new student[n_people];
 
     
-  }
-  
-  
+    for (int i = 0; i < n_people; i++) {
+        a[i].set_name(argv[i * 2 + 1]);
+        a[i].set_age(atoi(argv[i * 2 + 2]));
+    }
 
+    
+    int min_age = a[0].get_age();
+    for (int i = 1; i < n_people; i++) {
+        if (a[i].get_age() < min_age) {
+            min_age = a[i].get_age();
+        }
+    }
 
+    
+    cout << "The youngest person/people (Age " << min_age << "):" << endl;
+    for (int i = 0; i < n_people; i++) {
+        if (a[i].get_age() == min_age) {
+            a[i].display(); 
+        }
+    }
+
+    delete[] a; 
+    return 0;
+}
--- student.h
+++ student.h
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class student{
+class student {
 private:
-
-  string name;
+    string name;
+    int age;
 
 public:
-  
-   void set_name(string="Yme");
-    void print_name();
-
+    void set_name(string n = "Yme") { name = n; }
+    void set_age(int a) { age = a; }
+    int get_age() { return age; }
+    
+    
+    void display() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
 };
